d138d074a2b841cf3494af674edeccf3f9544977
Vova Shkil <vshkil@gmail.com>
2019-03-03 13:13:14 -0500

    new db instance

---
+++ db/data/tinderdb_public_messages.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.messages (id, from_userid, to_userid, message, sendtime) VALUES (1, 16, 8, 'Hello Gabi', '2019-02-28 06:26:10.243867');
+INSERT INTO public.messages (id, from_userid, to_userid, message, sendtime) VALUES (2, 8, 16, 'Hola Diego', '2019-02-28 06:26:10.288086');
+INSERT INTO public.messages (id, from_userid, to_userid, message, sendtime) VALUES (3, 16, 8, 'Hello Gabi', '2019-02-28 06:27:07.455126');
+INSERT INTO public.messages (id, from_userid, to_userid, message, sendtime) VALUES (4, 8, 16, 'Hola Diego', '2019-02-28 06:27:07.498329');
+INSERT INTO public.messages (id, from_userid, to_userid, message, sendtime) VALUES (5, 16, 8, 'Hello Gabi', '2019-02-28 06:30:15.189588');
+INSERT INTO public.messages (id, from_userid, to_userid, message, sendtime) VALUES (6, 8, 16, 'Hola Diego', '2019-02-28 06:30:15.232842');
